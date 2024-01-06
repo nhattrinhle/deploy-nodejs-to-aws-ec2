6c52e5dd76ed8567cdfdf28670656c64fcacfcbc
Trinh Le <nhattrinh.le.it@gmail.com>
2024-01-07 03:43:34 +0700

    #FEAT/API-register: create user model

---
+++ src/models/user.model.js
@@ -0,0 +1,93 @@
+const { Schema, model } = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+
+const USER_DOCUMENT_NAME = 'user'
+const USER_COLLECTION_NAME = 'users'
+const SALT_ROUND = 10
+
+const addressSchema = new Schema(
+    {
+        province: String,
+        district: String,
+        ward: String,
+        road: String,
+        note: String,
+        phone: String
+    },
+    {
+        _id: false
+    }
+)
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Invalid email')
+                }
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 8,
+            validate(value) {
+                if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+                    throw new Error('Password must contain at least one letter and one number')
+                }
+            },
+            private: true
+        },
+        fullName: String,
+        roles: [String],
+        address: [addressSchema],
+        isActive: {
+            type: Boolean,
+            default: false
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false
+        },
+        emailVerificationCode: String
+    },
+    {
+        timestamps: true,
+        collection: USER_COLLECTION_NAME
+    }
+)
+/**
+ * Check if email is taken
+ * @param {string} email
+ * @returns {Promise<boolean}
+ */
+userSchema.statics.isEmailTaken = async function (email) {
+    return !!(await this.findOne({ email }).lean())
+}
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, SALT_ROUND)
+    }
+    next()
+})
+
+/**
+ * @typedef User
+ */
+const User = model(USER_DOCUMENT_NAME, userSchema)
+
+module.exports = User
