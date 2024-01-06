7eaa3d1708f8163c2e161a6a557668ff21a956d2
Trinh Le <nhattrinh.le.it@gmail.com>
2024-01-06 21:30:07 +0700

    #FEAT: handle unexpected error

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const logger = require('./src/config/logger')
 
 const PORT = config.port
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     logger.info(`Server is running on PORT::${PORT}`)
 })
+
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            logger.info('Server closed')
+            process.exit(1)
+        })
+    } else {
+        process.exit(1)
+    }
+}
+
+const unexpectedErrorHandler = (error) => {
+    logger.info(error)
+    exitHandler()
+}
+
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
