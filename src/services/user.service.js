8fe061bcfb39e342bcfcd3c2944341487e8bd3a6
Trinh Le <nhattrinh.le.it@gmail.com>
2024-01-07 20:24:33 +0700

    #FEAT/API-register: create tokens

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,17 +1,30 @@
 const { User } = require('../models')
 const { BadRequestError } = require('../core/error.response')
+const { generateUserToken } = require('./token.service')
 
 /**
  * Create a user
  * @param {Object} userBody
- * @returns {Promise<User>}
+ * @returns {Promise<User, Token>}
  */
 const createUser = async (userBody) => {
     const isEmailTaken = await User.isEmailTaken(userBody.email)
     if (isEmailTaken) {
         throw new BadRequestError('Email already taken')
     }
-    return User.create(userBody)
+
+    const newUser = await User.create(userBody)
+    if (!newUser) {
+        throw new BadRequestError('Register failed')
+    }
+
+    const { _id: userId, email } = newUser
+    const tokens = await generateUserToken(userId, email)
+    if (!tokens) {
+        throw new BadRequestError('Creating tokens failed')
+    }
+
+    return { newUser, tokens }
 }
 
 module.exports = {
