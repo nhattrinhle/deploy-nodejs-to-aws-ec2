8a7595b6e4613ea4178d53c8c05c74b1be196497
NhatTrinhLe <nhattrinh.le.it@gmail.com>
2024-01-12 01:03:14 +0700

    #FEAT/API-verifyEmail: generate verify email token

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,14 @@
 const { Types } = require('mongoose')
+const { v4: uuidv4 } = require('uuid')
+const bcrypt = require('bcrypt')
+
+const generateVerifyEmailToken = async (_id) => {
+    const uniqueString = uuidv4() + _id
+    const roundsSalt = 10
+    const hashUniqueString = await bcrypt.hash(uniqueString, roundsSalt)
+
+    return { uniqueString, hashUniqueString }
+}
 
 const getExistingKeysInObject = (object, keys) => {
     return keys.reduce((newObj, key) => {
@@ -13,6 +23,7 @@ const getExistingKeysInObject = (object, keys) => {
 const convertToObjectId = (_id) => new Types.ObjectId(_id)
 
 module.exports = {
+    generateVerifyEmailToken,
     getExistingKeysInObject,
     convertToObjectId
 }
